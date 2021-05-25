f3df8e919ebc282ec3ecad8c9559d398d950ab24
dylan <dylan1997@live.com.sg>
2021-05-25 16:12:02 +0800

    added custom files

---
+++ px4.sh
@@ -0,0 +1,10 @@
+cd ./PX4-Autopilot
+DONT_RUN=1 make px4_sitl_default gazebo
+source ~/catkin_ws/devel/setup.bash    
+source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
+
+# launches gazebo_ros, mavros, sitl and px4 with custom vehicle, world and pose
+roslaunch px4 mavros_posix_sitl.launch vehicle:=typhoon_h480 world:=$(pwd)/../worlds/hills.world x:=-91.7 y:=4.56 z:=21
+

7285a5b1dc5f0fedd9eedaca939648db56130d5a
Dylan <dylan1997@live.com.sg>
2021-07-08 15:48:00 +0800

    Disabled gazebo and camera gui for better performance

--- px4.sh
+++ px4.sh
@@ -6,7 +6,7 @@ export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
 
 # launches gazebo_ros, mavros, sitl and px4 with custom vehicle, world and pose
-# roslaunch px4 mavros_posix_sitl.launch vehicle:=typhoon_h480
+# roslaunch px4 mavros_posix_sitl.launch
 # roslaunch px4 mavros_posix_sitl.launch world:=$(pwd)/../worlds/hills.world x:=-25.7 y:=27 z:=-1.2
 # roslaunch px4 multi_uav_mavros_sitl.launch world:=$(pwd)/Tools/sitl_gazebo/worlds/sonoma_raceway.world
 roslaunch px4 multi_uav_mavros_sitl.launch world:=$(pwd)/../worlds/custom_map.world
